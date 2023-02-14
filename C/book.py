877d0e030743c5ebfa41c95f24f9fd1461ec60e7
karen <klisse22@jaumebalmes.net>
2023-02-14 02:32:46 +0100

    getsetphyton

--- C/book.py
+++ C/book.py
@@ -1,7 +1,7 @@
 #crear una clase de nombre book que tenga un constructor y atributos (mínimo 6),
 #Getters/Setters
 #Mètodo de nombre info donde se muestren, por pantalla, todos los datos (atributos) de book.
-class book0:
+class book:
     def __init__(self, titulo, año, paginas, autor, editorial, premios):
         self.titulo = titulo
         self.año = año
@@ -14,32 +14,37 @@ class book0:
         return self.titulo
     def setTitulo(self, titulo):
         self.titulo = titulo
+
     def getAño(self):
         return self.año
     def setAño(self, año):
         self.año = año
+
     def getPaginas(self):
         return self.paginas
     def setPaginas(self, paginas):
         self.paginas = paginas
+
     def getAutor(self):
         return self.autor
     def setAutor(self, autor):
         self.autor = autor
+
     def getEditorial(self):
         return self.editorial
     def setEditorial(self, editorial):
         self.editorial = editorial
+
     def getPremios(self):
         return self.premios
     def setPremios(self, premios):
         self.premios = premios
 
-    def info(self):
-        print("Se llama: " + set.titulo)
+    def salutacio(self):
+        print("El nombre es: " + set.titulo)
         print("Publicado en el año" + set.año)
-        print("Tiene" + set.paginas)
+        print("Tiene" + set.paginas + "paginas")
         print("Su autor es " + set.autor)
         print("la editorial es " + set.editorial)
-        print("Tiene" + set.premios)
+        print("Tiene" + set.premios + "premios")
 
--- C/university.py
+++ C/university.py
@@ -1,7 +1,7 @@
 #crear una clase de nombre university que tenga un constructor, atributos (mínimo 6),
 #Getters/Setters
 #Mètodo de nombre info donde se muestren, por pantalla, todos los datos (atributos) de book.
-class university0:
+class university:
     def __init__(self, nombre, biblioteca, clases, alumnos, profesores, materias):
         self.nombre = nombre
         self.biblioteca = biblioteca
@@ -17,40 +17,34 @@ class university0:
 
     def getBiblioteca(self):
         return self.biblioteca
-
     def setBiblioteca(self, biblioteca):
         self.biblioteca = biblioteca
 
     def getClases(self):
         return self.clases
-
     def setClases(self, clases):
         self.clases = clases
 
     def getAlumnos(self):
         return self.alumnos
-
     def setAlumnos(self, alumnos):
         self.alumnos = alumnos
 
     def getProfesores(self):
         return self.profesores
-
     def setProfesores(self, profesores):
         self.profesores = profesores
 
     def getMaterias(self):
         return self.materias
-
     def setMaterias(self, materias):
         self.materias = materias
 
-    def info1(self):
+    def salutacio(self):
         print("El nombre es: " + self.nombre)
-        print("Consta de" + self.biblioteca)
-        print("Tiene" + self.clases)
-        print("Tiene " + self.alumnos)
-        print("Tiene " + self.profesores)
-        print("Consta de " + self.materias)
-
+        print("Consta de" + self.biblioteca + "bibliotecas")
+        print("Tiene" + self.clases + "clases")
+        print("Tiene " + self.alumnos + "alumnos")
+        print("Tiene " + self.profesores + "profesores")
+        print("Tiene " + self.materias + "materias")
 
